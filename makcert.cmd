c8749e88307aeb49f0f5ef7d892b79b10f9c9e76
j evans <g4@novadsp.com>
2024-01-12 15:28:48 +0000

    Set prompts in script

--- makcert.cmd
+++ makcert.cmd
@@ -1,21 +1,22 @@
 rem
-rem
+rem Thanks! 
+rem https://www.digitalocean.com/community/tutorials/openssl-essentials-working-with-ssl-certificates-private-keys-and-csrs
 rem
 
+rem rootca. used by client
 openssl genrsa -out certs/rootca.key 4096
-
-rem Root. used by client
-openssl req -x509 -new -nodes -key certs/rootca.key -days 20000 -out certs/rootca.pem
+openssl req -x509 -new -nodes -key certs/rootca.key -days 20000 -out certs/rootca.pem -subj "/C=GB/ST=London/L=London/O=CompanyName/CN=root"
 
 openssl genrsa -out certs/user.key 4096
-openssl req -new -key certs/user.key -out certs/user.csr
+openssl req -new -key certs/user.key -out certs/user.csr -subj "/C=GB/ST=London/L=London/O=CompanyName/CN=server"
 
-openssl x509 -req -in user.csr -CA certs/rootca.pem -CAkey certs/rootca.key -CAcreateserial -out certs/user.pem -days 20000
+openssl x509 -req -in certs/user.csr -CA certs/rootca.pem -CAkey certs/rootca.key -CAcreateserial -out certs/user.pem -days 20000
 
+rem final check will fail.
 openssl verify -CAfile certs/rootca.pem certs/rootca.pem
 openssl verify -CAfile certs/rootca.pem certs/user.pem
 openssl verify -CAfile certs/user.pem certs/user.pem
 
-rem for exchange
+rem for exchange. this is slw to generate
 openssl dhparam -out certs/dh4096.pem 4096
 
